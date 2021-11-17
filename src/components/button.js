66d3023aa72f9c0c14a53ac2f0f842ceb7c818d9
Vikas Shukla <vikasshukhla@gmail.com>
2021-11-17 16:50:16 +0530

    Initial commit

---
+++ src/components/button.js
@@ -0,0 +1,14 @@
+
+import PropTypes from 'prop-types'
+
+const button = props => {
+    return (
+        <button className='btn'>Add</button>
+    )
+}
+
+button.propTypes = {
+
+}
+
+export default button
