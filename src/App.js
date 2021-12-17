ce002acdffc6ab58b161cefcb43ba457b8973a4a
Vikas Shukla <vikasshukhla@gmail.com>
2021-12-17 20:39:44 +0530

    feat: added delete task

--- src/App.js
+++ src/App.js
@@ -25,15 +25,22 @@ function App() {
     },
   ])
 
-  //delete Task
   const deleteTask = (id) =>{
+    // console.log('Delete', id);
     setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) =>{
+    setTasks(tasks.map((task) => 
+      task.id === id? {...task, reminder:!task.reminder} : task
+      )
+    )
   }
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
     </div>
   );
 }
